efa1edaad21550e6a2bc193cfd2623ede488867a
AlanLake <alan.lake@mail.ru>
2021-02-16 15:42:40 +0300

    potential fix

--- db.php
+++ db.php
@@ -5,7 +5,6 @@
     // $user = 'root';
     // $pass = '';
     // $charset = 'utf8mb4';
-mysql://b579f94d90714d:c0a0634d@eu-cdbr-west-03.cleardb.net/heroku_6a09d810afc907f?reconnect=true
     $host = 'eu-cdbr-west-03.cleardb.net';
     $db = 'heroku_6a09d810afc907f';
     $user = 'b579f94d90714d';

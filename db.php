e08ebc1c4b9665d647cef868fd84606e08d193d2
AlanLake <alan.lake@mail.ru>
2021-02-16 15:57:03 +0300

    f

--- db.php
+++ db.php
@@ -13,5 +13,9 @@
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 
-$pdo = new PDO($dsn, $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    try {$pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+        throw new PDOException($e->getMessage());
+    }
